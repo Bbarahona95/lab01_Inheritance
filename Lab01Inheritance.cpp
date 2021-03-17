383aad419989a582f38025a298e2a52ec1b457ce
benba <benba4895@gmail.com>
2021-03-16 22:57:42 -0600

    -Se unificaron las clases Derived1 y Derived2, ya que hacian lo mismo y se pasan las frases por parametro desde el main

--- Lab01Inheritance.cpp
+++ Lab01Inheritance.cpp
@@ -4,26 +4,19 @@ struct Base {
     virtual void do_something() {}
 };
 
-struct Derived1 : Base {
-    virtual void do_semothing() {
-        std::cout << "Derived1!!!" << std::endl;
+struct Derived : Base {
+    virtual void do_something(std::string frase) {
+        std::cout << frase << std::endl;
     }
 };
-
-struct Derived2 : Base {
-    virtual void do_semothing() {
-        std::cout << "Derived2!!!" << std::endl;
-    }
-};
-
 // function main begins program execution
 int main(int argc, const char *argv[]) {
     std::cout << "Welcome to the UNA!" << std::endl;
 
-    Derived1 derived1;
-    derived1.do_semothing();
+    Derived derived1;
+    derived1.do_something("Derived1!!!");
 
-    Derived2 derived2;
-    derived2.do_semothing();
+    Derived derived2;
+    derived2.do_something("Derived2!!!");
 
 }  // end function main
