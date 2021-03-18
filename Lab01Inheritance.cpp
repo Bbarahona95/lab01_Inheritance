dcbd9f4bb44da723b801dd019c4e6aec7bd6dfa6
benba <benba4895@gmail.com>
2021-03-18 00:27:27 -0600

    -Se agrega una salida de pantalla en el metodo de la clase Base para probar funcionamiento de herendia en las clases derivadas.
    -Se elimino el virtual del metodo de la clase Derived, ya que no hay ninguna otra clase que derive de esta y no lo necesita.
    
    -Se crearon salidas de pantalla de las pruebas del metodo de la clase Base.

--- Lab01Inheritance.cpp
+++ Lab01Inheritance.cpp
@@ -1,12 +1,14 @@
 #include <iostream>  // allows program to output data to the screen
 
 struct Base {
-    virtual void do_something() {}
+    virtual void do_something() {
+        std::cout << "Execute do_something" << std::endl;
+    }
 };
 
 struct Derived : Base {
-    virtual void do_something(std::string frase) {
-        std::cout << frase << std::endl;
+    void do_somethingElse(std::string phrase) {
+        std::cout << phrase << std::endl;
     }
 };
 // function main begins program execution
@@ -14,9 +16,11 @@ int main(int argc, const char *argv[]) {
     std::cout << "Welcome to the UNA!" << std::endl;
 
     Derived derived1;
-    derived1.do_something("Derived1!!!");
+    derived1.do_something();
+    derived1.do_somethingElse("Derived1!!!");
 
     Derived derived2;
-    derived2.do_something("Derived2!!!");
+    derived2.do_something();
+    derived2.do_somethingElse("Derived2!!!");
 
 }  // end function main
